08af1ac7d5b05e5c01313e52f6067d436f933d5c
Aldhanekaa <aldhanekadev@gmail.com>
2025-08-14 03:21:36 +0700

    make light as default

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
           enableSystem={true}
           attribute="class"
           storageKey="theme"
-          defaultTheme="system"
+          defaultTheme="light"
         >
           <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
             <div className="relative mx-auto w-full max-w-screen-md flex-1 px-4 pt-20">
